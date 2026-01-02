6403627616eeeae59f27eb1f12e2473b03011376
academic258 <1801869637@qq.com>
2026-01-02 18:34:17 +0800

    Update and rename ClientPlayerEntity.java to ClientPlayerMixin.java

--- src/main/java/com/academic/hidepointer/mixin/ClientPlayerMixin.java
+++ src/main/java/com/academic/hidepointer/mixin/ClientPlayerMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayerEntity.class)
-public class ClientPlayerMixin {
+public class ClientPlayerMixin {  // 类名与文件名一致
     @Inject(method = "tick", at = @At("HEAD"))
     private void onTick(CallbackInfo ci) {
         // 这里可以添加额外的每帧逻辑，如果需要
